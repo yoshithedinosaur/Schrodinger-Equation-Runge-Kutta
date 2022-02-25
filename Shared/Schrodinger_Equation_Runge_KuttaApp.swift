40652d51e98e2c897b713de701deeeb5347fefa2
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-02-25 14:02:58 -0600

    Initial Commit

---
+++ Shared/Schrodinger_Equation_Runge_KuttaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schrodinger_Equation_Runge_KuttaApp.swift
+//  Shared
+//
+//  Created by Yoshinobu Fujikake on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Schrodinger_Equation_Runge_KuttaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
